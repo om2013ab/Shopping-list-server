fcfbafe62897bf074dc2b82cce9fa5a032953107
om2013ab <60521259+om2013ab@users.noreply.github.com>
2021-10-14 01:38:56 +0800

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,44 @@
+package com.omarahmed
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    install(Routing){
+        static("/static") {
+            resources("static")
+        }
+    }
+}
+
