4a81acb0441ac46c5a1c4600e7512ff4ac349060
om2013ab <60521259+om2013ab@users.noreply.github.com>
2021-10-16 19:46:59 +0800

    edit BASE_URL

--- src/util/Constants.kt
+++ src/util/Constants.kt
@@ -2,7 +2,7 @@ package com.omarahmed.util
 
 object Constants {
     const val DATABASE_NAME = "shoppingList_db"
-    const val BASE_URL = "http://localhost:8080/"
+    const val BASE_URL = "http://192.168.1.2:8080/"
     const val DEFAULT_PAGE_SIZE = 20
 
     const val ITEMS_PICTURE_PATH = "build/resources/main/static/items_pictures/"
