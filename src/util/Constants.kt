36af5dc3a318ea2195d103254b46f827b4e72415
om2013ab <omarbashaiwth@gmail.com>
2022-05-13 08:11:33 +0800

    update dependencies

--- src/util/Constants.kt
+++ src/util/Constants.kt
@@ -2,8 +2,6 @@ package com.omarahmed.util
 
 object Constants {
     const val DATABASE_NAME = "shoppingList_db"
-    const val BASE_URL = "http://localhost:1996/"
     const val DEFAULT_PAGE_SIZE = 10
 
-    const val ITEMS_PICTURE_PATH = "build/resources/main/static/items_pictures/"
 }
